027088e605016e8a48680be6844aca647e766f2b
Jason Cartwright <jason@gomalomo.com>
2025-12-17 15:31:50 -0600

    feat(api): add json api

--- lib/tunez/music/album.ex
+++ lib/tunez/music/album.ex
@@ -1,9 +1,17 @@
 defmodule Tunez.Music.Album do
-  use Ash.Resource, otp_app: :tunez, domain: Tunez.Music, data_layer: AshPostgres.DataLayer
+  use Ash.Resource,
+    otp_app: :tunez,
+    domain: Tunez.Music,
+    data_layer: AshPostgres.DataLayer,
+    extensions: [AshJsonApi.Resource]
 
   @min_year 1950
 
   def next_year, do: Date.utc_today().year + 1
+
+  json_api do
+    type "album"
+  end
 
   postgres do
     table "albums"
@@ -44,13 +52,17 @@ defmodule Tunez.Music.Album do
 
     attribute :name, :string do
       allow_nil? false
+      public? true
     end
 
     attribute :year_released, :integer do
       allow_nil? false
+      public? true
     end
 
-    attribute :cover_image_url, :string
+    attribute :cover_image_url, :string do
+      public? true
+    end
 
     create_timestamp :inserted_at
     update_timestamp :updated_at
